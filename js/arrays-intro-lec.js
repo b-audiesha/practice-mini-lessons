306b1c05ee2c7926ff39df72f39f9c28cd8a7258
audiesha brattin <a.brattin@gmail.com>
2021-01-04 12:03:47 -0600

    adding more notes

--- js/arrays-intro-lec.js
+++ js/arrays-intro-lec.js
@@ -3,9 +3,9 @@
 console.log([variable[0]]) will return first value
 console.log([variable][9]) if there is nothing at 9, it will return undefined or testArray[""]*/
 
-let instructor = ['David,Justin,Casey,Cody The Duck'];
-
+let instructor = ['David','Justin','Casey','Cody The Duck'];
 let randomIndex = Math.floor(Math.random() * instructor.length);
+
 alert("One of your instructors is named: " + instructor[randomIndex]); /*This gives you a randomized instructor from the instructors array.*/
 /*Math.random will give you a number between 0 and 1*/
 console.log('Howdy ' + instructor); /*= Howdy, David,Justin,Casey...*/
@@ -13,20 +13,24 @@ console.log('Howdy ' + instructor); /*= Howdy, David,Justin,Casey...*/
 /*How to access all the elements of the array*/
 
 for (let i = 0; i < instructor.length; i++ ){
-  console.log("One of your instructors is named :" + instructor[i]);
+  console.log("One of your instructors is named : " + instructor[i]);
 }
 /*using .length will iterate threw all of the array, runs the entire length.*/
 
-/*
-for ( let k = 0; k<= instructors.length; i ++){
-  console.log("One of your instructors is named :" + instructors[i]);
+// Note that we wanted i to increment only with it was less than the length of our instructor array, but not until it was equal to it.
+// Remember that an array is zero indexed, meaning our arrays start at the index of zero, and continue until an index point one number
+// lower than the length of the array. This saves us from accessing an undefined index, and performing an undesired behavior, as seen
+// below.
 
-}
-*/
+for (let k = 0; k <= instructor.length; k++) {
+    console.log("One of your instructors is named: " + instructor[k]);
+ }
+
 
 /*the less than or equal to creates an undefined value since an array begins at 0*/
+let falsyValues = [false, null, "", 0, undefined, NaN];
 
-/*function testAllForFalsy(array) {
+function testAllForFalsy(array) {
   for (let j = 0; j < array.length; j++){
     if(array[j]) {
       alert('Truthy value detected at the index: ' +j);
@@ -35,7 +39,7 @@ for ( let k = 0; k<= instructors.length; i ++){
   }
   alert('All values of array are falsy. ');
   return -1;
-}*/
+}
 
 /*For each loop to iterate threw array...->*/
 
@@ -44,15 +48,26 @@ for ( let k = 0; k<= instructors.length; i ++){
 })*/
 /*element(what we are accessing)//(index point)(requires the second but represents array itself)*/
 
+/*(Even)*/
+
 instructor.forEach(function(instructor,index, array) {
   if (index % 2 !== 0){
-    console.log('Instructor # ' + (index + 1) + 'of' + array.length + 'is named:' + instructor )
+    console.log('Instructor # ' + (index + 1) + ' of ' + array.length + ' is named : '+ instructor )
   }
 });
+
+/*(Odd)*/
+
+instructor.forEach(function(instructor,index, array) {
+  if (index % 2 === 0){
+    console.log('Instructor # ' + (index + 1) + ' of ' + array.length + ' is named : '+ instructor )
+  }
+});
+
 /*or the simplified version is*/
 
 instructor.forEach(function(instructor) {
-  console.log('One of your instructors names are :' + instructor)
+  console.log('One of your instructors names are : ' + ' ' + instructor)
 });
 /* if the index is not divisible by 2 then console.log every even instructor...(every other instructor)*/
 
@@ -65,9 +80,29 @@ numbers.forEach(function(number, index) {
 });
 
 /*
-refactor to for loop and access every other element instead of every element
+refactor to for loop and access every other element instead of every element produces same result
 */
 
 for(let y = 0; y < numbers.length; y += 2) {
   console.log(numbers[y] * 2);
 }
+/*
+-1 = last element of the array*/
+
+var coolArray = ["Snowboarding", "Playing Chess", "Telling The People You Care About That They Matter To You", "Dancing"];
+
+var coolerArray = ["Pizza", "Sunlight", "Chris Evans"];
+
+coolArray.forEach(function(coolThing) {
+  console.log("You know what's cool? " + coolThing + ".");
+})
+
+// Let's say that we wanted to use a defined function for this forEach loop!
+
+function youKnowCool(coolThing) {
+  console.log("You know what's cool? " + coolThing + ".");
+}
+
+coolArray.forEach(youKnowCool);
+
+coolerArray.forEach(youKnowCool);
