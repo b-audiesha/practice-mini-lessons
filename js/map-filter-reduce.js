4fb3bd2488119d91417d784fddda1762e052162c
audiesha brattin <a.brattin@gmail.com>
2021-02-23 10:21:00 -0600

    move

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -4,4 +4,5 @@ let mappedNumArr = numArr.map(function(num) {
 return num + num;
 })
 
+console.log(numArr); //returns original array
 console.log(mappedNumArr); //this logs a new array [2,4,6,8,10,12]
